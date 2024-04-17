a60a72a02e493f82366252b1e3d72b84f8c06464
AtakiSouma <atakisouma394@gmail.com>
2024-04-18 03:33:33 +0700

    create & list courase

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slice/authSlice";
 import roleSlice from "./slice/roleSlice";
 import userSlice from "./slice/userSlice";
+import courseSlice from "./slice/courseSlice";
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     role: roleSlice,
     user: userSlice,
+    course: courseSlice,
   },
 });
 
