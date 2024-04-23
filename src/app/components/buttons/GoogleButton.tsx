3f0c9114499379ba5213a1acb2da3baea2a03df6
ChonkySeals <trivm182003@gmail.com>
2024-04-23 13:11:00 +0700

    Fix login

--- src/app/components/buttons/GoogleButton.tsx
+++ src/app/components/buttons/GoogleButton.tsx
@@ -35,7 +35,7 @@ export function GoogleLoginButton() {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      console.log("result:", result)
+      console.log("result:", result);
       const { data }: { data: UserData } = await baseApi.post(
         "/api/v1/auth/login-with-Google",
         {
@@ -44,7 +44,7 @@ export function GoogleLoginButton() {
           photo: result.user.photoURL,
         }
       );
-      console.log(data)
+      console.log(data);
       const { link, access_token, ...user } = data.data;
       localStorage.setItem("access_token", access_token);
       localStorage.setItem("user", JSON.stringify(user));
@@ -52,7 +52,22 @@ export function GoogleLoginButton() {
       console.log("data from login Google:", data);
       dispatch(loginSuccessWithGoogle(data));
       console.log(data.data.link);
-      navigate(data.data.link);
+      if (
+        (user.user.isCertified === "Yes" &&
+          user.user.isVerified === true &&
+          user.user.role === "6615424b73f8eddb58cfe6ac") ||
+        user.user.role === "66153c6d09d7c5006797e0a3"
+      ) {
+        navigate(data.data.link);
+      } else if (
+        user.user.isCertified === "Proccessing" &&
+        user.user.isVerified === false &&
+        user.user.role === "6615424b73f8eddb58cfe6ac"
+      ) {
+        navigate("/wait");
+      } else {
+        navigate("/start");
+      }
     } catch (error) {
       console.log("Could not login with Google ", error);
       dispatch(loginFailure("Unknown error"));
--- src/app/pages/wait/WaitPage.tsx
+++ src/app/pages/wait/WaitPage.tsx
@@ -1,12 +1,30 @@
+import { CheckCircleOutlined } from "@ant-design/icons";
 import React from "react";
 
 const WaitPage = () => {
+  // https://i.etsystatic.com/40957748/r/il/e4efaa/4810125152/il_fullxfull.4810125152_23ir.jpg
+  const backgroundImageUrl =
+    "https://i0.wp.com/halcyonrealms.com/blogpics/japgifB.gif?resize=500%2C291&ssl=1";
   return (
-    <div className="bg-[#77B0AA] bg-gradient-to-r flex  from-[#77B0AA] to-[#135D66] items-center justify-center min-h-screen w-full">
+    <div
+      className="flex items-center justify-center min-h-screen w-full"
+      style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <div
-        className="grid grid-cols-5 relative shadow-md  drop-shadow-2xl rounded-sm bg-[#ffffff]
-      overflow-hidden w-[1200px] max-w-[100%] min-h-[480px] h-[65%] mx-10"
-      ></div>
+        className="flex flex-col gap-10 items-center justify-center relative shadow-md  drop-shadow-2xl rounded-sm bg-[#ffffff]
+      overflow-hidden w-[1200px] max-w-[100%] min-h-[480px] h-[65%] mx-96 p-28 text-center"
+      >
+        <div className="text-4xl">Your Application has been sent!</div>
+        <div>
+          It will take some time for us to review your application, please wait
+          for our response via email
+        </div>
+        <CheckCircleOutlined className="text-6xl text-[#77B0AA]" />
+      </div>
     </div>
   );
 };
